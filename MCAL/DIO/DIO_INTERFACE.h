1dd1d8a50db8095de0da49b98e10ff4252302b6a
MahmoudEssam10 <111686152+MahmoudEssam10@users.noreply.github.com>
2023-08-06 19:08:46 +0300

    DIO_INTERFACE.h

--- MCAL/DIO/DIO_INTERFACE.h
+++ MCAL/DIO/DIO_INTERFACE.h
@@ -7,6 +7,30 @@
 #define LOW 0
 #define PORT_OUTPUT 255
 #define PORT_INPUT 0
+typedef enum {
+	
+	Port_A,
+	Port_B,
+	Port_C,
+	Port_D
+	
+	
+	
+} et_Port;
+
+typedef enum {
+	Pin_0,
+	Pin_1,
+	Pin_2,
+	Pin_3,
+	Pin_4,
+	Pin_5,
+	Pin_6,
+	Pin_7
+	
+	
+}et_Pin;
+
 void DIO_VidSetPinDirection(u8 port, u8 pin, u8 dir);
 void DIO_VidSetPortDirection(u8 port, u8 dir);
 void DIO_VidSetPinValue(u8 port, u8 pin, u8 val);
